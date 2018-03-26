59490bc28788bf46add37478be762943bbe03833
Unknown <lauren.k.muhar@gmail.com>
2018-03-26 23:07:53 +1100

    fix up view results screen

--- client/app/enter-results/enter-results.component.ts
+++ client/app/enter-results/enter-results.component.ts
@@ -1,71 +1,95 @@
-import { ToastComponent } from './../shared/toast/toast.component';
-import { FormGroup, FormControl, Validators, FormBuilder, FormArray } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import { ToastComponent } from "./../shared/toast/toast.component";
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+  FormArray
+} from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
 
-import { RoundService } from '../services/round.service';
+import { RoundService } from "../services/round.service";
 
 @Component({
-    selector: 'app-enter-tips',
-    templateUrl: './enter-results.component.html',
-    styleUrls: ['./enter-results.component.scss']
+  selector: "app-enter-tips",
+  templateUrl: "./enter-results.component.html",
+  styleUrls: ["./enter-results.component.scss"]
 })
-
 export class EnterResultsComponent implements OnInit {
+  public rounds = [];
+  public selectedRound = { games: [] };
+  public isLoading = true;
+  public number = new FormControl("", Validators.required);
+  public isNew = true;
 
-    public rounds = [];
-    public selectedRound =  { games: []};
-    public selectedRoundId = null;
-    public isLoading = true;
-    public number = new FormControl('', Validators.required);
-    public isNew = true;
+  public selectForm: FormGroup;
+  public enterResultsForm: FormGroup;
 
-    public selectForm: FormGroup;
-    public enterResultsForm: FormGroup;
+  constructor(
+    public toast: ToastComponent,
+    private roundService: RoundService,
+    private formBuilder: FormBuilder
+  ) {}
 
-    constructor(
-        public toast: ToastComponent,
-        private roundService: RoundService,
-        private formBuilder: FormBuilder
-    ) {}
+  public ngOnInit() {
+    this.roundService.getRoundWithIdNumber().subscribe(
+      res => {
+        this.rounds = res;
+      },
+      error => console.log(error),
+      () => (this.isLoading = false)
+    );
 
-    public ngOnInit() {
-        this.roundService.getRoundWithIdNumber().subscribe((res) => {
-            this.rounds = res;
-        }, error => console.log(error),
-        () => this.isLoading = false);
+    this.selectForm = this.formBuilder.group({
+      number: this.number
+    });
 
-        this.selectForm = this.formBuilder.group({
-            number: this.number
+    this.enterResultsForm = this.formBuilder.group({
+      results: this.formBuilder.array([])
+    });
+
+    this.selectForm.valueChanges.subscribe(change => {
+      this.getSelectedRoundData(change.number);
+    });
+  }
+
+  private getSelectedRoundData(id) {
+    this.isLoading = true;
+    this.roundService.getRound(id).subscribe(
+      res => {
+        this.selectedRound = res;
+
+        this.enterResultsForm.reset();
+
+        const control = <FormArray>this.enterResultsForm.controls["results"];
+        res.games.forEach(game => {
+          control.push(new FormControl(game.result, Validators.required));
         });
+      },
+      error =>
+        this.toast.setMessage(
+          `Retrieve tips failed due to: ${error}`,
+          "warning"
+        ),
+      () => (this.isLoading = false)
+    );
+  }
 
-        this.enterResultsForm = this.formBuilder.group({
-            results: this.formBuilder.array([])
-        });
+  public saveResults() {
+    let i = 0;
+    this.selectedRound.games.forEach(game => {
+      game.result = this.enterResultsForm.value.results[i];
+      i++;
+    });
 
-        this.selectForm.valueChanges.subscribe((change) => {
-            this.getSelectedRoundData(change.number);
-        });
-    }
-
-    private getSelectedRoundData(id) {
-        this.isLoading = true;
-        this.roundService.getRound(id).subscribe((res) => {
-            this.selectedRound = res;
-            this.selectedRoundId = res._id;
-
-            const control = <FormArray>this.enterResultsForm.controls['results'];
-            res.games.forEach((game) => {
-                control.push(new FormControl(null, Validators.required));
-            });
-        }, error => console.log('Error', error),
-        () => this.isLoading = false);
-    }
-
-    public saveResults() {
-        let i = 0;
-        this.selectedRound.games.forEach((game) => {
-            game.result = this.enterResultsForm.value.results[i];
-            i++;
-        });
-    }
+    this.roundService.editRound(this.selectedRound).subscribe(
+      res => {
+        console.log(res);
+        this.toast.setMessage("Results successefully entered", "success");
+      },
+      error =>
+        this.toast.setMessage(`Save tips failed due to: ${error}`, "warning"),
+      () => (this.isLoading = false)
+    );
+  }
 }
