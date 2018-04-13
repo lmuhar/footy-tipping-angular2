10c0c6ea68a8f4c57e6ba9e89c175d0dd393ab8b
AGL\A140793 <A140793@agl.com.au>
2018-04-13 14:02:21 +1000

    add scraped ladder data

--- server/controllers/afl-ladder.ts
+++ server/controllers/afl-ladder.ts
@@ -5,23 +5,52 @@ export default class AflLadderCtrl {
 
     getAflLadderData = (req, res) => {
 
-        const url = 'https://muhar-footy-tipping-2.herokuapp.com/';
+        const url = 'http://www.fanfooty.com.au/game/ladder.php';
 
         request(url, (err, response, html) => {
             if (!err) {
                 const $ = cheerio.load(html);
-                let team;
-                const json = {team: '', points: ''};
+                const list = [];
 
-                $('.card-header').filter(() => {
-                    const data = $(this);
-                    console.log('data', data);
-                    team = data.text();
-
-                    json.team = team;
-                    console.log(team);
+                $('tbody').find('tr').each((i, elem) => {
+                    const data = {
+                        order: i,
+                        name: '',
+                        played: 0,
+                        wins: 0,
+                        draw: 0,
+                        loss: 0,
+                        for: 0,
+                        agt: 0,
+                        percent: 0,
+                        points: 0
+                    };
+                    $(elem).find('td').each((t, element) => {
+                        if (t === 0) {
+                            data.name = $(element).text();
+                        } else if (t === 1) {
+                            data.played = $(element).text();
+                        } else if (t === 2) {
+                            data.wins = $(element).text();
+                        } else if (t === 3) {
+                            data.draw = $(element).text();
+                        } else if (t === 4) {
+                            data.loss = $(element).text();
+                        } else if (t === 5) {
+                            data.for = $(element).text();
+                        } else if (t === 6) {
+                            data.agt = $(element).text();
+                        } else if (t === 7) {
+                            data.percent = $(element).text();
+                        } else if (t === 8) {
+                            data.points = $(element).text();
+                        }
+                    });
+                    list.push(data);
                 });
-                res.json(json);
+                list.splice(0, 1);
+                list.splice(8, 1);
+                res.json(list);
             }
         });
     }
