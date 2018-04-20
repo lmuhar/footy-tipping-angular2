51d6f3f1ce15f7ba0e347c60153300e154a5ce15
Lauren <lauren.k.muhar@gmail.com>
2018-04-21 01:50:01 +1000

    add round tips

--- client/app/ladder/ladder.component.ts
+++ client/app/ladder/ladder.component.ts
@@ -1,45 +1,62 @@
-import { Observable } from 'rxjs/Observable';
-import { Component, OnInit } from '@angular/core';
+import { Observable } from "rxjs/Observable";
+import { Component, OnInit } from "@angular/core";
 
-import { RoundService } from './../services/round.service';
-import { TipService } from './../services/tip.service';
-import { UserService } from '../services/user.service';
-import { ImageHelper } from './../utils/helpers/imageHelper';
+import { RoundService } from "./../services/round.service";
+import { TipService } from "./../services/tip.service";
+import { UserService } from "../services/user.service";
+import { ImageHelper } from "./../utils/helpers/imageHelper";
 
-import { forkJoin } from 'rxjs/observable/forkJoin';
+import * as _ from "lodash";
 
 @Component({
-  selector: 'app-ladder',
-  templateUrl: './ladder.component.html',
-  styleUrls: ['./ladder.component.scss']
+  selector: "app-ladder",
+  templateUrl: "./ladder.component.html",
+  styleUrls: ["./ladder.component.scss"]
 })
 export class LadderComponent implements OnInit {
   public users = [];
-  public roundData = { number: null, id: null};
+  public roundData = { number: null, id: null };
   public isLoading = true;
-  public html = null;
+  public roundTotal = null;
 
-  constructor(private userService: UserService,
-              private roundService: RoundService,
-              private tipService: TipService) {}
+  constructor(
+    private userService: UserService,
+    private roundService: RoundService,
+    private tipService: TipService
+  ) {}
 
   public ngOnInit() {
-    this.userService.getUserTotal().subscribe(res => {
-      this.users = res;
-      console.log('TESTed', this.users)
-    }, error => console.log(error),
-    () => this.isLoading = false);
+    this.userService.getUserTotal().subscribe(
+      res => {
+        this.users = res;
+      },
+      error => console.log(error),
+      () => (this.isLoading = false)
+    );
 
-    this.roundService.getRoundTotal().subscribe(res => {
-      if (res[0] && res[0]._id) {
+    this.roundService.getRoundTotal().subscribe(
+      res => {
+        if (res[0] && res[0]._id) {
           this.roundData = res[0]._id;
-          this.tipService.allTipsForRound(this.roundData.id).subscribe(result => {
-              console.log('TEST', result);
-          }, error => console.log(error),
-          () => this.isLoading = false);
-      }
-  }, error => console.log(error),
-  () => this.isLoading = false);
+          this.tipService.allTipsForRound(this.roundData.id).subscribe(
+            result => {
+              this.roundTotal = result;
+              this.users.map(item => {
+                let found = _.find(this.roundTotal, r => {
+                  return r.user_data.username === item.username;
+                });
+                item.roundTotal = found.total;
+                return item;
+              });
+            },
+            error => console.log(error),
+            () => (this.isLoading = false)
+          );
+        }
+      },
+      error => console.log(error),
+      () => (this.isLoading = false)
+    );
   }
 
   public returnUserImage(name) {
