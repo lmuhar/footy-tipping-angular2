4bfc1a1402e66e11f41a54c09261d63c1c1b895b
AGL\A140793 <A140793@agl.com.au>
2020-02-05 13:02:04 +1100

    remove cat things

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Angular2FullStackPage } from './app.po';
-import { } from 'jasmine';
+import {} from 'jasmine';
 
 describe('angular2-full-stack App', () => {
   let page: Angular2FullStackPage;
@@ -11,7 +11,6 @@ describe('angular2-full-stack App', () => {
   it('should display the navbar correctly', () => {
     page.navigateTo();
     expect<any>(page.getNavbarElement(0)).toEqual('Home');
-    expect<any>(page.getNavbarElement(1)).toEqual('Cats');
     expect<any>(page.getNavbarElement(2)).toEqual('Login');
     expect<any>(page.getNavbarElement(3)).toEqual('Register');
   });
